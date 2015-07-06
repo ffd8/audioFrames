f2af248dc42bf382a097b2ea06ae1958ec90073c
ffd8 <teddavis@ffd8.local>
2015-07-06 02:53:00 +0200

    initial commit
    compiles and runs fine on 10.6.8 + 10.8.5, issue saving tempaudio.wav
    file on 10.7.5?? Need to using ofSoundBuffer instead.

---
+++ src/ofApp.cpp
@@ -0,0 +1,346 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofBackground(0,0,0);
+    recording=false;
+    
+    //PATH
+    /*
+    string path = "~/Documents/audioFrames/";
+    ofDirectory dir(path);
+    if(!dir.exists()){
+        dir.create(true);
+    }
+    dir.setWriteable(true);
+    */
+    
+    //AUDIO
+    bufferSize = 1024;
+    left.assign(bufferSize, 0.0);
+    right.assign(bufferSize, 0.0);
+    volHistory.assign(400, 0.0);
+    
+    bufferCounter	= 0;
+    drawCounter		= 0;
+    smoothedVol     = 0.0;
+    scaledVol		= 0.0;
+    ofSoundStreamSetup(0,NUM_CHANNELS,this, SAMPLE_RATE,BUFFER_SIZE,4);
+    //tempAudio = path+"tempAudio.wav";
+    tempAudio = "../temp.wav";
+    
+    recTimer = 0;
+    recTimerEnd = 0;
+    recFrameRate = 40;
+    passFrameRate = recFrameRate;
+    recFrames = 160;
+    passFrames = recFrames;
+    
+    
+    //GUI
+    gui.setup(); // most of the time you don't need a name
+    gui.add(frames.setup("  FRAMES TO REC", 5, 1, 100));
+    gui.add(frameRate.setup("  FRAMES PER SEC", 25, 6, 60));
+    gui.add(recRecord.setup("[R]ECORD"));
+    gui.add(recPlay.setup("[P]LAY"));
+    gui.add(recLoop.setup("[L]OOP", false));
+    gui.add(recExport.setup("[S]AVE"));
+    recRecord.addListener(this,&ofApp::recRecordPressed);
+    //recPlay.addListener(this,&ofApp::recPlayPressed);
+    recExport.addListener(this,&ofApp::recExportPressed);
+    
+    //AUDIO
+    audioAmp = 1000.00;
+    soundStream.listDevices();
+    soundStream.setup(this, 0, 2, 44100, bufferSize, 4);
+    
+    ofHideCursor();
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::exit(){
+    recRecord.removeListener(this,&ofApp::recRecordPressed);
+    //recPlay.removeListener(this,&ofApp::recPlayPressed);
+    recExport.removeListener(this,&ofApp::recExportPressed);
+}
+
+/*
+//--------------------------------------------------------------
+void ofApp::frameRateChanged(int & frameRate){
+    passFrameRate = float(frameRate);
+    recDuration();
+}
+
+//--------------------------------------------------------------
+void ofApp::framesChanged(int & frames){
+    passFrames = float(frames);
+    recDuration();
+}
+
+void ofApp::recDuration(){
+    recFrameRate = float(1/float(frameRate)*1000);
+    recFrames = float(frames*recFrameRate);
+    cout << recFrames << endl;
+}
+*/
+//--------------------------------------------------------------
+void ofApp::recRecordPressed(){
+    cout<<"Start recording\n";
+    temp.stop();
+    recLoop = false;
+    if(ofFile::doesFileExist(tempAudio, true)){
+        ofFile::removeFile(tempAudio, true);
+    }
+    cout << tempAudio <<"----\n";
+    audioRecorder.setup(tempAudio); // (pt)
+    audioRecorder.setFormat(SF_FORMAT_WAV | SF_FORMAT_PCM_16);
+    recording=true;
+}
+//--------------------------------------------------------------
+void ofApp::recPlayPressed(){
+    temp.play();
+}
+//--------------------------------------------------------------
+void ofApp::recExportPressed(){
+    
+}
+
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    //AUDIO
+    if(useAudio){
+        //lets scale the vol up to a 0-1 range
+        scaledVol = ofMap(smoothedVol, 0.0, 0.17, 0.0, 1.0, true);
+        
+        //lets record the volume into an array
+        volHistory.push_back( scaledVol );
+        
+        //if we are bigger the the size we want to record - lets drop the oldest value
+        if( volHistory.size() >= 400 ){
+            volHistory.erase(volHistory.begin(), volHistory.begin()+1);
+        }
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    
+    
+    ofBackground(200);
+    
+    //timer 0.04 = 1/25
+    // = 40millis
+    
+    /*
+    if(recLoop){
+        if(!temp.getIsPlaying()){
+        temp.play();
+        }
+    }else{
+        temp.stop();
+    }
+     */
+    
+     if(recLoop){
+         temp.setLoop(true);
+     }else{
+         temp.setLoop(false);
+     }
+    
+    
+    recFrameRate = float(1/float(frameRate)*1000);
+    recFrames = float(frames*recFrameRate);
+    
+    if(ofGetElapsedTimeMillis() > recTimer + recFrames && recording){
+        cout<<"Stop recording\n";
+        recording=false;
+        audioRecorder.finalize();
+        audioRecorder.close();
+        temp.loadSound(tempAudio);
+        
+        
+        //recPlay = true;
+        temp.play();
+        temp.setVolume(1.0f);
+    }else if(!recording){
+        recTimer = ofGetElapsedTimeMillis();
+    }
+    /*
+    ofBackground(150,150,150);
+    ofSetColor(20,20,20);
+    ofDrawBitmapString("PRESS a to start recording",20,20);
+    ofDrawBitmapString("PRESS s to stop recording",20,40);
+    if(recording){
+        ofSetCircleResolution(50);
+        ofDrawBitmapString("RECORDING = TRUE",20,80);
+        ofSetColor(255,0,0);
+        ofCircle(400,160,150);
+    }
+    else
+        ofDrawBitmapString("RECORDING = FALSE",20,80);
+    */
+    
+    if(recording){
+        ofSetColor(255,0,0);
+    }else{
+        ofSetColor(255,255,255);
+    }
+    
+    ofPushMatrix();
+    ofTranslate(0, ofGetHeight()*.75);
+    ofNoFill();
+    ofSetLineWidth(2);
+    ofBeginShape();
+    for(int i = 0; i < bufferSize/2; i++){
+        float x = ofMap(i,0,bufferSize/2,0,ofGetWidth());
+        //float x = left[i]*audioAmp;
+        float y = left[i]*audioAmp;
+        
+        ofCurveVertex(x, y);
+    }
+    ofEndShape();
+    ofPopMatrix();
+
+
+    
+        gui.draw();
+    }
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    if(key=='r'){
+        recRecordPressed();
+    }else if(key == 'p'){
+        recPlayPressed();
+    }else if(key == 'l'){
+        if(!recLoop){
+            temp.play();
+            recLoop = true;
+        }else{
+            temp.stop();
+            recLoop = false;
+        }
+    }else if(key == 's'){
+        //"audio_"+ofToString(frames)+"_"+ofToString(frameRate)+".wav"
+        //ofFile f;
+        //f.open(ofToDataPath(tempAudio));
+        ofBuffer buf = ofBufferFromFile(tempAudio, false);
+        //string filePath = ofSystemSaveDialog("test", "Choose location for saving sample");
+        string tempPath = "audio_" + ofToString(int(frames)) + "_" + ofToString(int(frameRate)) + ".wav";
+        ofFileDialogResult saveFileResult = ofSystemSaveDialog(tempPath, "Save your file");
+        if (saveFileResult.bSuccess){
+            bool fileWritten = ofBufferToFile(saveFileResult.filePath, buf);
+            //processedImages[0].saveImage(saveFileResult.filePath);
+        }
+        
+        
+        
+        
+        /*
+         cout<<"Stop recording\n";
+         recording=false;
+         audioRecorder.finalize();
+         audioRecorder.close();
+         temp.loadSound(tempAudio);
+         temp.setVolume(0.75f);
+         //temp.setLoop(true);
+         */
+    }else if(key == 356){ // left arrow
+        int tempFrames = frames;
+        frames = tempFrames-1;
+        if(frames < 1){
+            frames = 1;
+        }
+    }else if(key == 358){ // right arrow
+        int tempFrames = frames;
+        frames = tempFrames+1;
+        if(frames > 100){
+            frames = 100;
+        }
+    }
+    cout << key << endl;
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+    ofShowCursor();
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+    //temp.play();
+    ofShowCursor();
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
+//--------------------------------------------------------------
+void ofApp::audioReceived(float * input, int bufferSize, int nChannels){
+    if(recording){
+        audioRecorder.addSamples(input,bufferSize*nChannels);
+    }
+    
+    float curVol = 0.0;
+    
+    // samples are "interleaved"
+    int numCounted = 0;
+    
+    //lets go through each sample and calculate the root mean square which is a rough way to calculate volume
+    if(input){
+        for (int i = 0; i < bufferSize; i++){
+            left[i]		= input[i*2]*0.5;
+            right[i]	= input[i*2+1]*0.5;
+            
+            curVol += left[i] * left[i];
+            curVol += right[i] * right[i];
+            numCounted+=2;
+        }
+    }
+    
+    //this is how we get the mean of rms :)
+    curVol /= (float)numCounted;
+    
+    // this is how we get the root of rms :)
+    curVol = sqrt( curVol );
+    
+    smoothedVol *= 0.93;
+    smoothedVol += 0.07 * curVol;
+    
+    bufferCounter++;
+
+}
+
+
+
+
---
+++ src/ofApp.h
@@ -0,0 +1,77 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxLibsndFileRecorder.h"
+#include "ofxGui.h"
+#define NUM_CHANNELS 1
+#define SAMPLE_RATE 44100
+#define BUFFER_SIZE 1024
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+        void exit();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+    
+        //AUDIO
+        void audioReceived(float * input, int bufferSize, int nChannels);
+        bool recording;
+        ofxLibsndFileRecorder audioRecorder;
+        string tempAudio;
+        ofSoundPlayer  temp;
+        Boolean pausePlayer;
+    
+    //AUDIO viz
+    bool useAudio;
+    bool avgAudio;
+    float audioAmp;
+
+    int bufferSize;
+    vector <float> left;
+    vector <float> right;
+    vector <float> volHistory;
+    
+    int 	bufferCounter;
+    int 	drawCounter;
+    
+    float smoothedVol;
+    float scaledVol;
+    
+    ofSoundStream soundStream;
+    
+    //TIMERS
+    float recTimer;
+    float recTimerEnd;
+    float passFrameRate, recFrameRate;
+    float passFrames, recFrames;
+    void recDuration();
+    
+    //GUI
+    ofxPanel gui;
+    ofxIntSlider frames;
+    void framesChanged(int & frames);
+    ofxIntSlider frameRate;
+    void frameRateChanged(int & frameRate);
+    ofxButton recRecord;
+    void recRecordPressed();
+    ofxButton recPlay;
+    ofxToggle recLoop;
+    void recPlayChanged();
+    void recPlayPressed();
+    ofxButton recExport;
+    void recExportPressed();
+
+		
+};
